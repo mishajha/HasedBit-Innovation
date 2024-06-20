1fe47ed7be2fa55be642cf375b55f75dc76c81a8
Misha kumari <90123791+mishajha@users.noreply.github.com>
2024-06-20 12:28:03 +0530

    Create q7.js

---
+++ Assignment_3/q7.js
@@ -0,0 +1,11 @@
+const students = [
+    { name: "Ram", scores: [80, 70, 60] },
+    { name: "Mohan", scores: [80, 70, 90] },
+    { name: "Sai", scores: [60, 70, 80] },
+    { name: "Hemang", scores: [90, 90, 80, 80] }
+];
+let result = students.map(student => {
+    let average = student.scores.reduce((acc, score) => acc + score, 0) / student.scores.length;
+    return { name: student.name, average: average };
+});
+console.log(result);
