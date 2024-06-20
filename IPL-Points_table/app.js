4b7aded2d05844cf3990b1533b2171b0ed6d6045
Misha kumari <90123791+mishajha@users.noreply.github.com>
2024-06-20 12:47:08 +0530

    Create app.js

---
+++ IPL-Points_table/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IplTable from './IplTable';
+
+function App() {
+  return (
+    <div className="App">
+      <IplTable />
+    </div>
+  );
+}
+
+export default App;
